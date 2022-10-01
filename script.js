9715fe7404fbeb52b526fae339b652469f22d183
Emily Santos <eyssantos@gmail.com>
2022-10-01 19:12:09 +0100

    toggle search input box when clicking search icon

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ function show(id) {
 function hide(id) {
   document.getElementById(id).style.display = 'none';
 }
+
+function displaySearch() {
+  let box = document.getElementById("search-input");
+  if (box.style.display === "none") {
+    box.style.display = "block";
+  } else {
+    box.style.display = "none";
+  }
+}
